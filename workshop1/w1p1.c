39d9f713f83d0f74693008c0f8db43086dd07f86
utsav gautam <121043963+UtsavHere@users.noreply.github.com>
2023-01-13 13:19:20 -0500

    Add files via upload
    workshop 1 added all the files

---
+++ workshop1/w1p1.c
@@ -0,0 +1,32 @@
+/*
+*****************************************************************************
+                          Workshop - #1 (P1)
+Full Name  : Utsav Gautam
+Student ID#: 157891219
+Email      : ugautam4@myseneca.ca
+Section    : ZCC
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+// entry point to the application
+int main (void){
+
+    printf("Workshop 1 Part-1\n"); 
+    printf("=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n") ;
+    printf("(standard input output) library function!\n\n") ; 
+    printf("Dear teacher,\n\n"); 
+    printf("    I promise I will work hard from this day onward.\n"); 
+    printf("    I acknowledge that practice is extremely important,\n"); 
+    printf("    so I will do all workshops, quizzes, and assignments.\n\n"); 
+    printf("Sincerely,\n\n");
+    printf("Utsav Gautam\n");
+    printf("Student ID# 157891219");
+
+    return 0; // return the program run without any error to the OS
+}
